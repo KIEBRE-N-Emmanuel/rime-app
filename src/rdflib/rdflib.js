dec275c47dbacc122f67e28fa4076d73cbc84926
unknown <nemmanuelkiebre@gmail.com>
2023-05-11 14:12:31 +0000

    V4

--- src/rdflib/rdflib.js
+++ src/rdflib/rdflib.js
@@ -1,10 +1,27 @@
-import * as rdf from 'rdflib';
+import  rdf, {Namespace} from 'rdflib';
 
-const store = rdf.graph();
-const fetcher = new rdf.Fetcher(store);
+const rdflib = () => {
 
-fetcher.load('RIME_04_0804.rdf').then(() => {
-  console.log('Loaded RDF graph:', store);
-}).catch((err) => {
-  console.error('Failed to load RDF graph:', err);
-});
+var RDF = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+var RDFS = Namespace("http://www.w3.org/2000/01/rdf-schema#")
+var FOAF = Namespace("http://xmlns.com/foaf/0.1/")
+var XSD = Namespace("http://www.w3.org/2001/XMLSchema#")
+
+rdf.sym('http://xmlns.com/foaf/0.1/knows')
+
+var foafKnows = FOAF('knows');
+var store = rdf.graph()
+var timeout = 5000 // 5000 ms timeout
+var fetcher = new rdf.Fetcher(store, timeout)
+
+fetcher.nowOrWhenFetched(url, function(ok, body, response) {
+    if (!ok) {
+        console.log("Oops, something happened and couldn't fetch data " + body);
+    } else if (response.onErrorWasCalled || response.status !== 200) {
+        console.log('    Non-HTTP error reloading data! onErrorWasCalled=' + response.onErrorWasCalled + ' status: ' + response.status)
+    } else {
+         console.log("---data loaded---")
+})
+};
+
+export default Metier;
--- src/sparql/metier.js
+++ src/sparql/metier.js
@@ -3,48 +3,53 @@ import * as rdf from 'rdflib';
 
 const Metier = () => {
   const [jobs, setJobs] = useState([]);
-
+//http://localhost:3000/RIME_11_0505.rdf
   useEffect(() => {
     const store = rdf.graph();
-    const ontologyURI = 'http://localhost:3000/RIME_04_0804.rdf';
-    const ontology = rdf.sym(ontologyURI);
+    const ontologyURI = 'http://localhost:3000/RIME_11_0505.rdf';
+    const ontology = store.sym(ontologyURI);
     
     rdf.parse(ontologyURI, store, ontology.uri, 'application/rdf+xml', (err, res) => {
       if (!err) {
-        console.log(store);
+        console.log('Fichier chargé avec succès',store);
       } else {
-        console.log(err);
+        console.log('Chargement du fichier échoué', err);
       }
     });
 
     const fetcher = new rdf.Fetcher(store);
     
     // Charge le fichier RDF dans le store
-    fetcher.load('http://localhost:3000/RIME_04_0804.rdf').then(() => {
+    fetcher.load('http://localhost:3000/RIME_11_0505.rdf').then(() => {
       console.log('Loaded RDF graph:', store);
   
       // Requête SPARQL pour récupérer les noms des métiers
       const query = `
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX ont: <http://www.semanticweb.org/kiebre_le_fariam/ontologies/2023/4/2/RIME_04_0756#>
+        PREFIX ont: <http://www.semanticweb.org/kiebre_le_fariam/ontologies/2023/4/11/untitled-ontology-11/>
         
-        SELECT ?metier
+        SELECT ?nom
         WHERE {
-            ?metier rdf:type ont:Métier .
+          ?individu rdf:type ont:OWLClass_341b4f72_1a08_4673_abb2_55cb0606edb8 ;
+                    ont:rdfs:label ?nom .
         }
         
         `;
   
-         // Exécute la requête SPARQL sur le store
-        const results = store.query(query);
-        console.log(results);
-        const jobs = results.map(result => result.metier.value);
-        setJobs(jobs); // Mise à jour du state avec les noms des métiers
+        // Exécute la requête SPARQL sur le store
+        store.query(query, (err, results) => {
+          if (err) {
+            console.error('Failed to execute SPARQL query:', err);
+          } else {
+            console.log('SPARQL query results:', results);
+            const jobs = results.map(result => result.get('?nom').value);
+            setJobs('resultat du Mise à jour du state avec les noms des métiers', jobs); // Mise à jour du state avec les noms des métiers
+          }
+        });
     }).catch((err) => {
       console.error('Failed to load RDF graph:', err);
     });
   }, []);
-  
 
   return (
     <div>
